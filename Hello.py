f6e6a7da740afe9695d9f19385724ace8d7a7f13
FishBomber <122764744+FishBomber@users.noreply.github.com>
2024-01-08 18:33:30 -0600

    ok

--- Hello.py
+++ Hello.py
@@ -1,7 +1,12 @@
 import streamlit as st
+from schoology import Schoology
+
 
 st.title("Schoology App")
 st.write("No idea what to do? Scroll to the instructions at the bottom of the page.")
 
 KEY = st.text_input("Enter your API key:")
 SECRET = st.text_input("Enter your secret:")
+
+client = Schoology(key=KEY, secret=SECRET)
+assignments = client.get('/v1/sections/{section_id}/assignments')
