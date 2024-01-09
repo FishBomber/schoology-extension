6a525da24f17141821035da0c358c8ef625a825a
FishBomber <122764744+FishBomber@users.noreply.github.com>
2024-01-08 19:25:08 -0600

    ok

--- Hello.py
+++ Hello.py
@@ -6,8 +6,17 @@ st.title("Schoology App")
 st.write("No idea what to do? Scroll to the instructions at the bottom of the page.")
 
 KEY = st.text_input("Enter your API key:")
-SECRET = st.text_input("Enter your secret:")
+SECRET = st.text_input("Enter your API secret:")
 
-# Two-legged
 sc = schoolopy.Schoology(schoolopy.Auth(KEY, SECRET))
+auth = schoolopy.Auth(KEY, SECRET)
 
+# Instantiate the API wrapper
+sc = schoolopy.Schoology(auth)
+
+# Get a list of all courses
+courses = sc.get_courses()
+
+# Print the name of each course
+for course in courses:
+    print(course['course_title'])
