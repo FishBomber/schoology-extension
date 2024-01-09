cf930f19f4ae1543f22802fb0e6ff72e49c1d39f
FishBomber <122764744+FishBomber@users.noreply.github.com>
2024-01-08 18:54:19 -0600

    ok

--- Hello.py
+++ Hello.py
@@ -7,3 +7,7 @@ st.write("No idea what to do? Scroll to the instructions at the bottom of the pa
 
 KEY = st.text_input("Enter your API key:")
 SECRET = st.text_input("Enter your secret:")
+
+# Two-legged
+sc = schoolopy.Schoology(schoolopy.Auth(KEY, SECRET))
+sc.get_feed()
