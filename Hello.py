1c6e89bdfeb5fcb2173cee0035167067bc13df47
FishBomber <122764744+FishBomber@users.noreply.github.com>
2024-01-08 18:29:58 -0600

    ok

--- Hello.py
+++ Hello.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
 st.title("Schoology App")
+st.write("No idea what to do? Scroll to the instructions at the bottom of the page.")
 
 KEY = st.text_input("Enter your API key:")
+SECRET = st.text_input("Enter your secret:")
