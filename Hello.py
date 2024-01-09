03f167e54d18185bc379ea883de0ed8a39249a89
FishBomber <122764744+FishBomber@users.noreply.github.com>
2024-01-08 18:48:49 -0600

    ok

--- Hello.py
+++ Hello.py
@@ -8,5 +8,5 @@ st.write("No idea what to do? Scroll to the instructions at the bottom of the pa
 KEY = st.text_input("Enter your API key:")
 SECRET = st.text_input("Enter your secret:")
 
-client = Schoology(key=KEY, secret=SECRET)
+client = schoology(key=KEY, secret=SECRET)
 assignments = client.get('/v1/sections/{section_id}/assignments')
